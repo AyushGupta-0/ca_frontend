d972cee8e5d9ba4f8e58f3314052cf8544090da9
Ayush <ayush5555.ag@gmail.com>
2023-09-29 15:30:35 +0530

    chnages

--- src/components/After Login/Billing App/reports/SaleReports.jsx
+++ src/components/After Login/Billing App/reports/SaleReports.jsx
@@ -44,7 +44,10 @@ const tableData = [
     
 ]
 
+
+
 const SaleReports = () => {
+    
     const navigate = useNavigate();
     const [searchQuery, setSearchQuery] = useState('');
     const [searchData, setSearchData] = useState(tableData);
@@ -62,8 +65,38 @@ const SaleReports = () => {
 
     const token = localStorage.getItem("token");
     const { firmId } = useSelector((store) => store.FirmRegistration);
+    console.log("firmid -", firmId)
     const { getAllInvoice } = useSelector((store) => store.invoiceReducer);
     console.log("🚀 ~ file: SaleReports.jsx:64 ~ SaleReports ~ getAllInvoice:", getAllInvoice)
+    // const listItems = getAllInvoice.map(
+    //     (element) => {
+    //         return (<div>
+               
+                   
+    //               <div>
+    //                 {element._id}
+    //                 </div>
+    //                 </div>)
+                    
+    //     }
+    // )
+    // console.log( "1 data are "  , listItems);
+    const TotalNumberOfTransaction = getAllInvoice.length;
+    console.log("NumberOfTransaction- " ,TotalNumberOfTransaction );
+
+    const TotalNumberOfSaleArray = getAllInvoice.map(option => option.finalAmount);
+    let sumofAmount = 0;
+    for(let i=0;i<TotalNumberOfSaleArray.length;i++){
+        sumofAmount += TotalNumberOfSaleArray[i];
+    }
+    console.log("SumofAmount - " , sumofAmount );
+
+    const TotalNumberOfDueArray = getAllInvoice.map(option => option.dueAmount);
+    let sumofDueAmount = 0;
+    for(let i=0;i<TotalNumberOfDueArray.length;i++){
+        sumofDueAmount += TotalNumberOfDueArray[i];
+    }
+    console.log("SumofDueAmount - " , sumofDueAmount );
 
     const dispatch = useDispatch();
 
@@ -153,7 +186,7 @@ const SaleReports = () => {
                         p='2'
                         >
                             <Text>No of Txn</Text>
-                            <Text fontSize='20px' mt='-2'>5</Text>
+                            <Text fontSize='20px' mt='-2'>{TotalNumberOfTransaction}</Text>
                         </Box>
                         <Box border= '0.1px solid lightgray'
                         boxShadow= 'rgba(149, 157, 165, 0.2) 0px 8px 24px'
@@ -163,7 +196,7 @@ const SaleReports = () => {
                         p='2'
                         >
                             <Text>Total Sale</Text>
-                            <Text fontSize='20px' mt='-2'>100 ₹</Text>
+                            <Text fontSize='20px' mt='-2'>{sumofAmount}₹</Text>
                         </Box>
                     <Box border= '0.1px solid lightgray'
                     boxShadow= 'rgba(149, 157, 165, 0.2) 0px 8px 24px'
@@ -173,7 +206,7 @@ const SaleReports = () => {
                     p='2'
                     >
                         <Text>Balance Due</Text>
-                        <Text fontSize='20px' mt='-2'>90 ₹</Text>
+                        <Text fontSize='20px' mt='-2'>{sumofDueAmount}</Text>
                     </Box>
                     </Flex>
                     <TableContainer m='2' margin='15px'
@@ -199,15 +232,16 @@ const SaleReports = () => {
                                 </Tr>
                             </Thead>
                             <Tbody>
-                                {tableData?.map((data)=>(
+                                {getAllInvoice?.map((data)=>(
                                     <Tr onClick={()=>handleRowClick(data.reportNo)} style={{cursor:'pointer'}}
                                     >
                                         <Td style={{border:'1px solid gray'}}>
                                         {data.reportNo}</Td>
-                                        <Td style={{border:'1px solid gray'}}>{data.customerName}</Td>
-                                        <Td style={{border:'1px solid gray'}}>{data.amount} ₹</Td>
-                                        <Td style={{border:'1px solid gray'}}>{data.balance} </Td>
-                                        <Td style={{border:'1px solid gray'}}>{data.date}</Td>
+                                        <Td style={{border:'1px solid gray'}}>{data.shipToCustomerName}</Td>
+                                        {/* <Td style={{border:'1px solid gray'}}></Td> */}
+                                        <Td style={{border:'1px solid gray'}}>{data.finalAmount} ₹</Td>
+                                        <Td style={{border:'1px solid gray'}}>{data.dueAmount} </Td>
+                                        <Td style={{border:'1px solid gray'}}>{data.dueDate}</Td>
                                         <Td style={{border:'1px solid gray'}}>Edit</Td>
                                         <Td style={{border:'1px solid gray'}}>Delete</Td>
                                     </Tr>
